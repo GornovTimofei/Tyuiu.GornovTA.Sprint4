47b7c4e545f321ca5df6290554a5d2527b42cb71
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-10-31 20:09:53 +0500

    Add project files.

---
+++ Tyuiu.GornovTA.Sprint4.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GornovTA.Sprint4.Task0.V30.Lib;
+
+namespace Tyuiu.GornovTA.Sprint4.Task0.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numArray = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            int res = ds.GetSumEvenArrEl(numArray);
+            int waitSum = 30;
+            Assert.AreEqual(waitSum, res);
+        }
+    }
+}
---
+++ Tyuiu.GornovTA.Sprint4.Task0.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint4.Task0.V30.Lib;
+
+namespace Tyuiu.GornovTA.Sprint4.Task0.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №4 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание №0                                                              *");
+            Console.WriteLine("* Вариант №30                                                             *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму четных   *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] array = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма чётных элементов массива = " + ds.GetSumEvenArrEl(array));
+            Console.ReadKey();
+        }
+    }
+}
