36269da3fa8a092a3f8ad9acaab000fe78feae99
Tyuiu.GornovTA@outlook.com <Tyuiu.GornovTA@outlook.com>
2023-11-03 19:39:13 +0500

    Task3

---
+++ Tyuiu.GornovTA.Sprint4.Task3.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GornovTA.Sprint4.Task3.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GornovTA.Sprint4.Task3.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79eaf854-a2a5-4617-8914-bd50bba27cd0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GornovTA.Sprint4.Task3.V2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GornovTA.Sprint4.Task3.V2.Lib;
+
+namespace Tyuiu.GornovTA.Sprint4.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrayValue = new int[5, 5] { { 4, 3, 2, 8, 7 },
+                                          { 4, 6, 5, 5, 7 },
+                                          { 3, 8, 8, 3, 5 },
+                                          { 5, 7, 5, 4, 4 },
+                                          { 8, 8, 3, 7, 6 } };
+            int rows = arrayValue.GetUpperBound(0) + 1;
+            int columns = arrayValue.Length / rows;
+            DataService ds = new DataService();
+            Console.Title = "Спринт №4 | Выполнил: Горнов Т. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт №4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание №3                                                              *");
+            Console.WriteLine("* Вариант №2                                                              *");
+            Console.WriteLine("* Выполнил: Горнов Тимофей Анатольевич | АСОиУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{arrayValue[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(arrayValue);
+            Console.WriteLine("Максимальный эелемент первой строки = " + res);
+            Console.ReadKey();
+        }
+    }
+}
